9ff508f1077dcb2d2910e68a848f394331a6d944
Luca Fabbri <luca.fabbri@zucchetti.it>
2025-08-31 15:15:54 +0200

    feat: adds default pre and post behavior and task when all publisher

---
+++ Concordia.Core/Behaviors/RequestPostProcessorBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Concordia.Contracts;
+
+namespace Concordia.Core.Behaviors;
+
+/// <summary>
+/// Behavior for executing all <see cref="IRequestPostProcessor{TRequest,TResponse}"/> instances after handling the request
+/// </summary>
+/// <typeparam name="TRequest">Request type</typeparam>
+/// <typeparam name="TResponse">Response type</typeparam>
+public class RequestPostProcessorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IRequestPostProcessor<TRequest, TResponse>> _postProcessors;
+
+    public RequestPostProcessorBehavior(IEnumerable<IRequestPostProcessor<TRequest, TResponse>> postProcessors)
+        => _postProcessors = postProcessors;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var response = await next(cancellationToken).ConfigureAwait(false);
+
+        foreach (var processor in _postProcessors)
+        {
+            await processor.Process(request, response, cancellationToken).ConfigureAwait(false);
+        }
+
+        return response;
+    }
+}
